070496862e24e4da2ab3511654054e937d5c0d6a
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-19 16:36:32 +0800

    restructure the project to web-app and web-model and the parent pom module. when mutiple modules for spring boot, then web-app Uber jar will include the web-model jar in its lib path. If the parent pom include spring-boot-maven-plugin, pay attention to the <spring-boot.repackage.skip>true property in web-model module pom, to avoid it become fat as well.

--- web-app/src/main/java/asg/dev/demospringboot01/controllers/ExampleController.java
+++ web-app/src/main/java/asg/dev/demospringboot01/controllers/ExampleController.java
@@ -1,5 +1,6 @@
 package asg.dev.demospringboot01.controllers;
 
+import asg.dev.demospringboot01.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,16 @@ public class ExampleController {
         Map<String, Object> returnVal = new HashMap<>();
         returnVal.put("hello", "world");
         return returnVal;
+    }
+
+    @RequestMapping("/user")
+    public Map<String, Object> getUser() {
+        User user01 = new User();
+        user01.setFirstName("Jack");
+        user01.setLastName("Ma");
+        user01.setEmail("jack.ma@aliyun.com");
+        Map<String, Object> res = new HashMap<>();
+        res.put("new user", user01);
+        return res;
     }
 }
