f918a9741716531d6b66490f3d3a4d3abaa9072c
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-19 16:57:05 +0800

    @PathVariable and @RequestParam to retrieve request parameters.

--- web-app/src/main/java/asg/dev/demospringboot01/controllers/ExampleController.java
+++ web-app/src/main/java/asg/dev/demospringboot01/controllers/ExampleController.java
@@ -1,7 +1,9 @@
 package asg.dev.demospringboot01.controllers;
 
 import asg.dev.demospringboot01.model.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,7 +18,28 @@ public class ExampleController {
         return returnVal;
     }
 
+    @RequestMapping("/user/{id}")
+    public Map<String, Object> getUserk(@PathVariable("id") Integer id) {
+        User user01 = new User();
+        user01.setFirstName("Jack-" + id);
+        user01.setLastName("Ma");
+        user01.setEmail("jack-" + id + ".ma@aliyun.com");
+        Map<String, Object> res = new HashMap<>();
+        res.put("new user", user01);
+        return res;
+    }
+
     @RequestMapping("/user")
+    public Map<String, Object> getUserp(@RequestParam("id") Integer id) {
+        User user01 = new User();
+        user01.setFirstName("Jack-" + id);
+        user01.setLastName("Ma");
+        user01.setEmail("jack-" + id + ".ma@aliyun.com");
+        Map<String, Object> res = new HashMap<>();
+        res.put("new user", user01);
+        return res;
+    }
+    @RequestMapping("/user0")
     public Map<String, Object> getUser() {
         User user01 = new User();
         user01.setFirstName("Jack");
