8d625a6ccce958a135c4bb06e7f892ab4bd29347
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-19 10:51:10 +0800

    leverage https://start.spring.io/ to prepare starter pom, as well comment out mvn because of running maven from WSL.

---
+++ src/main/java/asg/dev/demospringboot01/Demospringboot01Application.java
@@ -0,0 +1,13 @@
+package asg.dev.demospringboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospringboot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospringboot01Application.class, args);
+	}
+
+}
---
+++ src/test/java/asg/dev/demospringboot01/Demospringboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package asg.dev.demospringboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospringboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
