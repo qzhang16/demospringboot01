d2a2b5f5bf3b68187b55c7e385df099da540898f
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-19 12:26:07 +0800

    example controller to run this small demo as a fat jar with tomcat embeded.

---
+++ src/main/java/asg/dev/demospringboot01/controllers/ExampleController.java
@@ -0,0 +1,17 @@
+package asg.dev.demospringboot01.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ExampleController {
+    @RequestMapping("/")
+    public Map<String, Object> getIndex() {
+        Map<String, Object> returnVal = new HashMap<>();
+        returnVal.put("hello", "world");
+        return returnVal;
+    }
+}
